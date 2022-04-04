3a39c17f34620d4b077decd99d1bb6d430a75261
Platinum_ning <92506194+Platinumning@users.noreply.github.com>
2022-04-04 21:32:35 +0800

    4月4日代码练习

---
+++ c_4_4/c_4_4/test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//输入描述：
+//输入一个整数，范围在32位有符号整数范围内
+//输出描述：
+//输出这个整数
+//int main()
+//{
+//    int i = 0;
+//    scanf("%d", &i);
+//    printf("%d", i);
+//    return 0;
+//}
+
+//输入描述：
+//输入一个浮点数
+//输出描述：
+//输出一个浮点数，保留三位小数
+//int main()
+//{
+//    float a = 0;
+//    scanf("%f", &a);
+//    printf("%.3f", a);
+//    return 0;
+//}
+
+//输入描述：
+//输入一个字符，范围在ascii范围内
+//输出描述：
+//输出这个字符
+//int main()
+//{
+//    char a;
+//    scanf("%c", &a);
+//    printf("%c", a);
+//    return 0;
+//}
+
+//输入描述：
+//一行输入 3 个整数，用空格隔开。
+//输出描述：
+//请输出第二个整数的值。
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("%d", b);
+//    return 0;
+//}
+
+//输入描述：
+//一行读入一个 char 类型的字符。
+//输出描述：
+//输出这个字符组成的 3 * 3 矩形。
+//int main()
+//{
+//    char n;
+//    scanf("%c", &n);
+//    printf("%c%c%c\n", n, n, n);
+//    printf("%c%c%c\n", n, n, n);
+//    printf("%c%c%c\n", n, n, n);
+//    return 0;
+//}
+
+//输入描述：
+//输入一个char类型字符
+//输出描述：
+//输出一个用这个字符填充的对角线长5个字符，倾斜放置的菱形
+//int main()
+//{
+//    char n;
+//    scanf("%c", &n);
+//    printf("  %c\n", n);
+//    printf(" %c%c%c\n", n, n, n);
+//    printf("%c%c%c%c%c\n", n, n, n, n, n);
+//    printf(" %c%c%c\n", n, n, n);
+//    printf("  %c\n", n);
+//    return 0;
+//}
+
